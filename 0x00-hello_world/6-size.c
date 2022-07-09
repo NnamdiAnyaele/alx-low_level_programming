f964e9fe7633af98ee3c206ed39a7347fc537641
NnamdiAnyaele <nnamdianya2011@gmail.com>
2022-07-09 09:28:47 -0700

    print size of types in current computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,9 @@
 
 /**
  *
- * main - prints the size of various types
+ * main - Entry point
  * 
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 (Success)
  *
  */
 int main(void)
