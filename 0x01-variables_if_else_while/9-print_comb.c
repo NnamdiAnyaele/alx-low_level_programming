e68a9702282d023cf6c4c6d982aa8d65db6ad3d3
NnamdiAnyaele <nnamdianya2011@gmail.com>
2022-09-09 22:06:11 -0700

    combination of single numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all combinations of single digit numbers
+ * in ascending order followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+	int d = 48;
+
+	while (c < 58)
+	{
+		while (d < 58)
+		{
+			if (c != d && c < d)
+			{
+				putchar(c);
+				putchar(d);
+
+				if (!(d == 56 && a == 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d++
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
