da7fc2117690e22057c0e0d7f17be920434345f4
nnamdianyaele <nnamdianya2011@gmail.com>
2022-10-21 14:47:02 -0700

    print line of length passed integer

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * print_line - draws a straight line
+ *
+ * @n: length of line to be drawn
+ *
+ * Return: void.
+ *
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n')
+	else
+	{
+		int i;
+
+		i = 0;
+		while (i < n)
+		{
+			_putchar('_');
+		}
+	}
+}
